d09a5ca795d41723d7e161ecf0603f5d66b6d557
aldo278 <aantanolopez@linfield.edu>
2025-07-21 23:22:04 -0700

    showing navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
 
-    <router-outlet />
+    <div class="min-h-screen flex flex-col">
+      <app-header/>
+
+      <main class="flex-1">
+
+        <h1>Welcome to {{title}}!</h1>
+        <router-outlet />
+
+      </main>
+      
+      <app-footer />
+    </div>
   `,
-  styles: [],
+  styles: [
+   
+  ],
 })
 export class AppComponent {
   title = 'HandymanSite';
